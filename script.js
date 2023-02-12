243e6e6b5d174eba968021202f4f7e0b08eef399
MaxBulavchyk <maxblk555@gmail.com>
2023-02-13 00:47:34 +0100

    Start

---
+++ script.js
@@ -0,0 +1,17 @@
+function navToggle() {
+    var navToggleNav = document.getElementById("nav");
+    navToggleNav.classList.toggle("_active");
+    var html = document.getElementById("html");
+    html.classList.toggle("_overflow");
+    var body = document.getElementById("body");
+    body.classList.toggle("_overflow");
+}
+
+function navClose() {
+    var navToggleNav = document.getElementById("nav");
+    navToggleNav.classList.remove("_active");
+    var html = document.getElementById("html");
+    html.classList.remove("_overflow");
+    var body = document.getElementById("body");
+    body.classList.remove("_overflow");
+}
